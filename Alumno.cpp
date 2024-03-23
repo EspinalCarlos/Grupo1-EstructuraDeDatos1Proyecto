1b09c050e2603bf43cfffe1a8dd09caaae6258ac
Carlos Espinal <cespinalb@gmail.com>
2024-03-22 18:13:27 -0600

    que sexo

--- Alumno.cpp
+++ Alumno.cpp
@@ -3,6 +3,7 @@
 #include<iostream>
 #include <array>
 
+//Consturctor de la clase Alumno, solo tiene el nombre y el numero de cuenta ya que es lo unico que tiene la clase
 Alumno::Alumno(string name, int accNum){
     this->nombreAlumno = name; 
     this->numeroCuenta = accNum;
