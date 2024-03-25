c9f5c9fd04c241f578932da68741a316fe1402af
unknown <hdannysabillon992232@gmail.com>
2024-03-25 02:42:57 -0600

    solo unos colores, nada se rompio...creo

--- Estru1Proyecto-Grupo#1.cpp
+++ Estru1Proyecto-Grupo#1.cpp
@@ -1,7 +1,5 @@
-﻿
-#include <iostream>
+﻿#include <iostream>
 using namespace std;
-
 #include "Object.h"
 #include "TDALista.h"
 #include "ArrayList.h"
@@ -14,14 +12,20 @@ using namespace std;
 #include "LinkedStack.h"
 #include "ArrayStack.h"
 
+const string red = "\033[1;31m";
+const string green = "\033[1;32m";
+const string l_blue = "\033[1;36m";
+const string yellow = "\033[1;33m";
+const string purple = "\033[1;35m";
+const string white = "\033[0m";
 
 void menuPilas() {
     bool fin = true;
 
-    cout << "---------Stack---------" << endl;
+    cout << l_blue << "---------Stack---------" << endl;
 
     do {
-        cout << "1. Trabajar con ArrayStack \n"
+        cout << l_blue << "1. Trabajar con ArrayStack \n"
             << "2. Trabajar con LinkedStacked \n"
             << "3. Regresar al Menu Principal \n";
 
@@ -32,7 +36,7 @@ void menuPilas() {
             ArrayStack* pila = new ArrayStack();
             bool salir1 = true;
             do {
-                cout << "Operaciones del Stack: \n"
+                cout << purple << "Operaciones del Stack: \n"
                     << "1. Push \n"
                     << "2. Pop \n"
                     << "3. Peek \n"
@@ -45,7 +49,7 @@ void menuPilas() {
 
                 switch (seleccion1) {
                 case 1: {
-                    cout << "Ingrese un caracter: ";
+                    cout << purple << "Ingrese un caracter: ";
                     char linea;
                     cin >> linea;
                     char* c = &linea;
@@ -89,7 +93,7 @@ void menuPilas() {
             LinkedStack* pila = new LinkedStack();
             bool salir2 = true;
             do {
-                cout << "Operaciones del Stack: \n"
+                cout << purple << "Operaciones del Stack: \n"
                     << "1. Push \n"
                     << "2. Pop \n"
                     << "3. Peek \n"
@@ -102,7 +106,7 @@ void menuPilas() {
 
                 switch (seleccion2) {
                 case 1: {
-                    cout << "Ingrese un caracter: ";
+                    cout << purple << "Ingrese un caracter: ";
                     char linea;
                     cin >> linea;
                     char* c = &linea;
@@ -143,28 +147,28 @@ void menuPilas() {
 
         }
         else if (seleccion == 3) {
-            cout << "---------" << endl;
+            cout << white << "---------" << endl;
             fin = false;
         }
         else {
-            cout << "Opci�n no v�lida" << endl;
+            cout << red << "Opcion no valida" << endl;
         }
 
     } while (fin);
 }
 
-
 void menuPrincipal(){
-    cout << "---- MENU PRINCIPAL ----\n\n1.Trabajar con Listas\n2.Trabajar con Pilas\n3. Trabajar con Colas\n4. Salir\n\n";
+    cout << yellow << "---- MENU PRINCIPAL ----\n\n1.Trabajar con Listas\n2.Trabajar con Pilas\n3. Trabajar con Colas\n4. Salir\n\n";
 }
+
 void menuListas(){
-    cout << "---- Operaciones con Listas ----\n\n1. Insertar Elemento\n2.Imprimir Elementos\n3.Buscar Elemento\n4. Borrar Elemento\n5. Ver si esta vacia\n6. Obtener elemento por posicion\n7. Obtener Siguiente\n8. Obtener Anterior\n9. Borrar todos los elementos de la lista\n10. Regresar al menu anterior ";
+    cout << purple << "---- Operaciones con Listas ----\n\n1. Insertar Elemento\n2.Imprimir Elementos\n3.Buscar Elemento\n4. Borrar Elemento\n5. Ver si esta vacia\n6. Obtener elemento por posicion\n7. Obtener Siguiente\n8. Obtener Anterior\n9. Borrar todos los elementos de la lista\n10. Regresar al menu anterior ";
 }
 
 void menuColas() {
     int opQ;
     TDACola* cola = nullptr;
-    cout << "Menu Tipo de Cola\n\n1. Trabajar con ArrayQueue\n2. Trabajar con LinkedQueue\n3. Regresar al Menu Principal" << endl;
+    cout << l_blue << "Menu Tipo de Cola\n\n1. Trabajar con ArrayQueue\n2. Trabajar con LinkedQueue\n3. Regresar al Menu Principal" << endl;
     cin >> opQ;
     switch (opQ) {
     case 1:
@@ -176,7 +180,7 @@ void menuColas() {
     case 3:
         break;
     default:
-        cout << "OPCION INGRESADA NO VALIDA: INGRESE UN NUMERO VALIDO" << endl;
+        cout << red << "OPCION INGRESADA NO VALIDA: INGRESE UN NUMERO VALIDO" << endl;
         break;
     }
     if (opQ == 1 || opQ == 2) {
@@ -186,13 +190,13 @@ void menuColas() {
         Alumno* a = nullptr;
         Object* temp = nullptr;
         do {
-            cout << "Operaciones de Colas\n\n1. Encolar\n2. Desencolar\n3. Ver Frente\n4. Verificar si esta vacia\n5. Imprimir Elementos\n6. Borrar los elementos\n7. Regresar al Menu Anterior" << endl;
+            cout << purple << "Operaciones de Colas\n\n1. Encolar\n2. Desencolar\n3. Ver Frente\n4. Verificar si esta vacia\n5. Imprimir Elementos\n6. Borrar los elementos\n7. Regresar al Menu Anterior" << endl;
             cin >> opSubMenuQ;
             switch (opSubMenuQ) {
             case 1:
-                cout << "Ingrese el nombre completo del alumno: " << endl;
+                cout << purple << "Ingrese el nombre completo del alumno: " << endl;
                 cin >> nombre;
-                cout << "Ingrese el numero de cuenta del alumno: " << endl;
+                cout << purple << "Ingrese el numero de cuenta del alumno: " << endl;
                 cin >> numC;
                 a = new Alumno(nombre, numC);
                 cola->queue(a);
@@ -200,29 +204,29 @@ void menuColas() {
             case 2:
                 temp = cola->dequeue();
                 if (temp == nullptr) {
-                    cout << "La cola esta vacia" << endl;
+                    cout << red << "La cola esta vacia" << endl;
                 }
                 else {
                     a = dynamic_cast<Alumno*>(temp);
-                    cout << "El primer elemento en la cola es: " + a->toString() + " y se ha quitado de la cola" << endl;
+                    cout << green << "El primer elemento en la cola es: " + a->toString() + " y se ha quitado de la cola" << endl;
                 }                 
                 break;
             case 3:
                 temp = cola->peek();
                 if (temp == nullptr) {
-                    cout << "La cola esta vacia" << endl;
+                    cout << red << "La cola esta vacia" << endl;
                 }
                 else {
                     a = dynamic_cast<Alumno*>(temp);
-                    cout << "El primer elemento en la cola es: " + a->toString() << endl;
+                    cout << green << "El primer elemento en la cola es: " + a->toString() << endl;
                 }      
                 break;
             case 4:
                 if (cola->isEmpty()) {
-                    cout << "La cola esta vacia" << endl;
+                    cout << green << "La cola esta vacia" << endl;
                 }
                 else {
-                    cout << "La cola no esta vacia" << endl;
+                    cout << red << "La cola no esta vacia" << endl;
                 }
                 break;
             case 5:
@@ -232,10 +236,10 @@ void menuColas() {
                 cola->clear();
                 break;
             case 7:
-                cout << "Regresando al menu ... \n\n" << endl;
+                cout << white << "Regresando al menu ... \n\n" << endl;
                 break;
             default:
-                cout << "OPCION INGRESADA NO VALIDA: INGRESE UN NUMERO VALIDO" << endl;
+                cout << red << "OPCION INGRESADA NO VALIDA: INGRESE UN NUMERO VALIDO" << endl;
                 break;
             }
         } while (opSubMenuQ != 7);
@@ -262,7 +266,7 @@ int main(int argc, const char* argv[]){
         cin >> opcionMM;
         switch (opcionMM){
         case 1:
-            cout << "--- Menu Tipo Lista ---\n\n1. Array List\n2. Linked List\n\n Ingrese su opcion: ";
+            cout << l_blue << "--- Menu Tipo Lista ---\n\n1. Array List\n2. Linked List\n\n Ingrese su opcion: ";
             cin >> opcionListsP;
            switch(opcionListsP){
                 case 1:
@@ -281,15 +285,15 @@ int main(int argc, const char* argv[]){
                 switch (opcionLists) {
                 case 1:
                     do {
-                        cout << "--- Insertar Alumno a la Lista ---\nIngrese el nombre completo del Alumno: ";
+                        cout << purple << "--- Insertar Alumno a la Lista ---\nIngrese el nombre completo del Alumno: ";
                         cin.clear();
                         cin.ignore();
                         getline(cin, nombreA);
-                        cout << "\nIngrese el numero de cuenta del alumno: ";
+                        cout << purple << "\nIngrese el numero de cuenta del alumno: ";
                         getline(cin, numeroC);
                         cout << endl;
                         a = new Alumno(nombreA, numeroC);
-                        cout << "\nIngrese el indice al que desea insertar el alumno: ";
+                        cout << purple << "\nIngrese el indice al que desea insertar el alumno: ";
                         cin >> indiceInsert;
                         cout << endl;
                         if (indiceInsert <= lista->getSize()+1 && indiceInsert > 0) {
@@ -297,13 +301,13 @@ int main(int argc, const char* argv[]){
                         }
                         else {
                             while (indiceInsert > lista->getSize()+1 || indiceInsert <= 0) {
-                                cout << "Indice invalido porfavor ingrese un indice valido: ";
+                                cout << red << "Indice invalido porfavor ingrese un indice valido: ";
                                 cin >> indiceInsert;
                             }
                             lista->inserta(a, indiceInsert);
                         }
-                        cout << "\nAlumno insertado a la lista correctamente\n";
-                        cout << "\nDesea agregar otro alumno a la lista? (1.si 0. no): ";
+                        cout << green << "\nAlumno insertado a la lista correctamente\n";
+                        cout << green << "\nDesea agregar otro alumno a la lista? (1.si 0. no): ";
                         cin >> addLoop;
                         cout << endl;
 
@@ -311,35 +315,35 @@ int main(int argc, const char* argv[]){
                     } while (addLoop != 0);
                     break;
                 case 2:
-                    cout << "\n--- Impresion de Lista --- \n";
+                    cout << purple << "\n--- Impresion de Lista --- \n";
                     if (!lista->vacia())
                         lista->imprimeLista();
                     else
-                        cout << "La lista esta vacia.";
+                        cout << red << "La lista esta vacia.";
                     break;
                 case 3:
-                    cout << "\n--- Busqueda en la Lista ---\n";
-                    cout << "Ingrese el numero de cuenta del estudiante: ";
+                    cout << purple << "\n--- Busqueda en la Lista ---\n";
+                    cout << purple << "Ingrese el numero de cuenta del estudiante: ";
                     cin >> numeroC;
                     localizaNum = lista->localiza(new Alumno("", numeroC));
                     if (localizaNum != 0)
-                        cout << endl << "\nNombre del estudiante: " << dynamic_cast<Alumno*>(lista->recupera(lista->localiza(new Alumno("", numeroC))))->getNombre() << " Indice: " << lista->localiza(new Alumno("", numeroC)) << endl;
+                        cout << endl << purple << "\nNombre del estudiante: " << dynamic_cast<Alumno*>(lista->recupera(lista->localiza(new Alumno("", numeroC))))->getNombre() << " Indice: " << lista->localiza(new Alumno("", numeroC)) << endl;
                     else
-                        cout << "No existe alumno con tal numero\n\n";
+                        cout << red << "No existe alumno con tal numero\n\n";
                     break;
                 case 4:
-                    cout << "\n--- Eliminacion en la Lista ---\n" << "Ingrese la posicion del alumno en la lista el cual desea eliminar: ";
+                    cout << purple << "\n--- Eliminacion en la Lista ---\n" << "Ingrese la posicion del alumno en la lista el cual desea eliminar: ";
                     cin >> indiceDelete;
                     if (lista->getSize() > 0 && indiceDelete <= lista->getSize()) {
                         lista->suprime(indiceDelete);
-                        cout << "Alumno eliminado correctamente";
+                        cout << green << "Alumno eliminado correctamente";
                     }
                     else if (lista->vacia()) {
-                        cout << "No se pudo eliminar el alumno: LISTA VACIA";
+                        cout << red << "No se pudo eliminar el alumno: LISTA VACIA";
                     }
                     else {
                         do {
-                            cout << "No se pudo eliminar el alumno: INDICE INCORRECTO";
+                            cout << red << "No se pudo eliminar el alumno: INDICE INCORRECTO";
                             cin >> indiceDelete;
 
                         } while (indiceDelete > lista->getSize() || indiceDelete < 0);
@@ -347,51 +351,51 @@ int main(int argc, const char* argv[]){
                     }
                     break;
                 case 5:
-                    cout << "\n--- Ver si esta vacia la lista ---";
+                    cout << purple << "\n--- Ver si esta vacia la lista ---";
                     if (!lista->vacia()){
-                        cout << "\nLa lista no esta vacia, contiene " << lista->getSize() << " alumnos\n";
+                        cout << red << "\nLa lista no esta vacia, contiene " << lista->getSize() << " alumnos\n";
                     }
                     else {
-                        cout << "\nLa lista esta vacia.\n";
+                        cout << green << "\nLa lista esta vacia.\n";
                     }
                     break;
                 case 6:
-                    cout << "\n--- Obtener alumno ingresando indice ---\n";
-                    cout << "Ingrese el indice que desea obtener: ";
+                    cout << purple << "\n--- Obtener alumno ingresando indice ---\n";
+                    cout << purple << "Ingrese el indice que desea obtener: ";
                     cin >> indicePORINDEX;
                     if (indicePORINDEX > 0 && indicePORINDEX < lista->getSize())
-                        cout << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->recupera(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->recupera(indicePORINDEX))->getNumeroCuenta() << endl;
+                        cout << purple << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->recupera(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->recupera(indicePORINDEX))->getNumeroCuenta() << endl;
                     else
-                        cout << "Indice invalido";
+                        cout << red << "Indice invalido";
 
                     break;
                 case 7:
-                    cout << "\n--- Obtener siguiente de un indice ---\n";
-                    cout << "Ingrese el indice que desea obtener: ";
+                    cout << purple << "\n--- Obtener siguiente de un indice ---\n";
+                    cout << purple << "Ingrese el indice que desea obtener: ";
                     cin >> indicePORINDEX;
                     cout << endl;
                     if (indicePORINDEX > 0 && indicePORINDEX <= lista->getSize()-1)
-                        cout << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->siguiente(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->siguiente(indicePORINDEX))->getNumeroCuenta() << endl;
+                        cout << purple << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->siguiente(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->siguiente(indicePORINDEX))->getNumeroCuenta() << endl;
                     else
-                        cout << "Indice invalido";
+                        cout << red << "Indice invalido";
                     break;
                 case 8:
-                    cout << "\n--- Obtener anterior de un indice ---\n";
-                    cout << "Ingrese el indice que desea obtener: ";
+                    cout << purple << "\n--- Obtener anterior de un indice ---\n";
+                    cout << purple << "Ingrese el indice que desea obtener: ";
                     cin >> indicePORINDEX;
                     if (indicePORINDEX > 1 && indicePORINDEX <= lista->getSize())
-                        cout << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->anterior(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->anterior(indicePORINDEX))->getNumeroCuenta() << endl;
+                        cout << purple << "Nombre del Alumno: " << dynamic_cast<Alumno*>(lista->anterior(indicePORINDEX))->getNombre() << " Numero de Cuenta: " << dynamic_cast<Alumno*>(lista->anterior(indicePORINDEX))->getNumeroCuenta() << endl;
                     else
-                        cout << "Indice invalido";
+                        cout << red << "Indice invalido";
                     break;
                 case 9:
-                    cout << "\n--- Nulificacion de toda la lista ---\n";
+                    cout << purple << "\n--- Nulificacion de toda la lista ---\n";
                     if (!lista->vacia()){
                         lista->anula();
-                        cout << "\nLista anulada correctamente\n";
+                        cout << green << "\nLista anulada correctamente\n";
                     }
                     else {
-                        cout << "\nLa lista esta vacia, no hay nada que anular\n";
+                        cout << red << "\nLa lista esta vacia, no hay nada que anular\n";
                     }
                     break;
                 case 10:
